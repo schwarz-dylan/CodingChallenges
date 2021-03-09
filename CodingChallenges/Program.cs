510d14eb5b0b05a4b852158ea2256f2f6d3f18e1
schwarz-dylan <schwarz_dylan@yahoo.com>
2021-03-08 20:49:43 -0600

    completed this hw project

---
+++ CodingChallenges/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0; //defining sum as an int that = 0
+            for (int i = 1; i < 1000; i++) //counting by i or 1 until we hit 1000, i++ = the update; its counting all the way 1000 for every single intiger. Which is the condition that is i < 1000
+            {
+                int x = i % 3; //if it = 0 then we know its a multiple of 3 or 5 below
+                int y = i % 5;
+                if (x == 0 || y == 0)
+                {
+                    //Console.Write("{0}  ", i);
+                    sum = sum + i; //adding all of the i's that equal 0 which then adds up to our answer
+                }
+            }
+            Console.WriteLine("Multiples of 3 and 5 in 1000 = {0}\n", sum);
+            Console.Read();
+
+        }//end main
+
+
+    }//end class
+
+
+}//end namepsace
---
+++ Multiples3And5Library/Multiples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5Library
+{
+    public class Multiples
+    {
+
+
+    }//end class
+}//end namespace
