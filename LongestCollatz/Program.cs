3224b59830f5d2ea0fe97fa9b49bf00f7f71e9e5
schwarz-dylan <schwarz_dylan@yahoo.com>
2021-03-12 08:12:44 -0600

    completed new assignment

---
+++ LongestCollatz/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCollatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The following iterative sequence is defined for the set of positive integers:
+
+            //n → n / 2(n is even)
+            //n → 3n + 1(n is odd)
+
+            //Using the rule above and starting with 13, we generate the following sequence:
+            //13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+            //It can be seen that this sequence(starting at 13 and finishing at 1) contains 10 terms.Although it has not been proven yet, it is thought that all starting numbers finish at 1.
+
+            //Which starting number, under one thousand, produces the longest chain ?
+
+            //NOTE : Once the chain starts the terms are allowed to go above one thousand.
+
+
+            //------------------------------------------------------------------------------------------------//
+
+
+            //int n = 0;
+            //int nLength = 0;
+            //int seq;
+
+            //for (int i = 2; i < length; i++)
+            //{
+
+            //}
+
+            long sLength = 0;
+            long startNumber = 0;                        
+            int n;
+            
+
+            for (int i = 2; i <= 1000; i++) //starting a loop that goes to 1000 --start with 2
+            {
+                int length = 1; //the end result
+                n = i;
+                while (n != 1)
+                {
+
+
+                    if ((n % 2) == 0)
+                    {                           //this if statment finds out if the number is even or else run to see if its odd
+                        n = n / 2;
+                    }//end if
+
+
+
+                    else
+                    {
+                        n = n * 3 + 1;          //if statment finds out if the number is odd
+                    }//else
+                    length++;  //this is a increment operator (increments up by one each time)
+
+
+
+                }//end while
+
+                
+                if (length > sLength)
+                {
+                    sLength = length;
+
+                    startNumber = i;
+
+                }//end if
+
+                
+            }//for
+
+            
+
+            Console.WriteLine($"Longest chain : {sLength}\nStarting number: {startNumber}");
+            
+
+
+
+        }//end main
+    }//end class
+}//end namespace
---
+++ LongestCollatz/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LongestCollatz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("LongestCollatz")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc81bdea-1561-46ec-a73c-28e363f4568f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
